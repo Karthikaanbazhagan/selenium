6285f08e60048e616a97b93dd52dc67a17214f04
Karthikaanbazhagan <128590583+Karthikaanbazhagan@users.noreply.github.com>
2023-03-22 18:06:51 +0530

    Update HandlingFrames.java

--- HandlingFrames.java
+++ HandlingFrames.java
@@ -1,19 +1,37 @@
 package TestNG.Frames;
-
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class HandlingFrames {
+public class FrameHandlingExample {
 
     public static void main(String[] args) {
         
-        System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "https://www.codido.co/");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.example.com");
-        driver.switchTo().frame(0);
+
+        
+        WebElement iframe = driver.findElement(By.tagName("iframe"));
+        driver.switchTo().frame(iframe);
+
+        
+        WebElement input = driver.findElement(By.tagName("input"));
+        input.sendKeys("Hello, world!");
+
+        
         driver.switchTo().defaultContent();
-        driver.switchTo().frame("frame-name-or-id");
-        driver.switchTo().defaultContent();
+
+       
+        WebElement button = driver.findElement(By.tagName("button"));
+        button.click();
+
+        
         driver.quit();
     }
+
 }
+
+
+   

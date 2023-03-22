ee62d80700000282cb8983f315a3e320d97d1e70
Karthikaanbazhagan <128590583+Karthikaanbazhagan@users.noreply.github.com>
2023-03-22 17:59:47 +0530

    Update MultiWindow.java

--- MultiWindow.java
+++ MultiWindow.java
@@ -7,28 +7,20 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class MultipleWindowsExample {
 
     public static void main(String[] args) {
-        // set system property for ChromeDriver
+       
         System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
-
-        // create ChromeDriver instance
         WebDriver driver = new ChromeDriver();
-
-        // navigate to website with multiple windows
         driver.get("https://www.example.com");
 
-        // get window handle of current window
+
         String currentWindowHandle = driver.getWindowHandle();
 
-        // locate link that opens new window
+        
         WebElement link = driver.findElement(By.linkText("Open New Window"));
-
-        // click link to open new window
         link.click();
-
-        // get all window handles
         Set<String> windowHandles = driver.getWindowHandles();
 
-        // loop through all window handles and switch to new window
+    
         for (String windowHandle : windowHandles) {
             if (!windowHandle.equals(currentWindowHandle)) {
                 driver.switchTo().window(windowHandle);
@@ -36,13 +28,10 @@ public class MultipleWindowsExample {
             }
         }
 
-        // do some actions on new window
-        // ...
-
-        // switch back to original window
+        
         driver.switchTo().window(currentWindowHandle);
 
-        // close driver instance
+        
         driver.quit();
     }
 }
